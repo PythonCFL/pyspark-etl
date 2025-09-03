520327f22d00e745e4f97d97f8eab5afdccfdc15
cflorente <cflorente@minsait.com>
2025-09-03 16:07:35 +0200

    fix tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,6 +6,9 @@ Se ejecutan con pytest.
 """
 
 import pytest
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from functions import generar_nombre_grupo
 
 def test_generar_nombre_grupo_dev():
